7682e9313c17b0242567fb4e7c7ddb3d14b82a11
viktorbjo <viktor.kung@live.se>
2023-12-06 17:31:53 +0100

    my first commit

---
+++ Core/Inc/test.h
@@ -0,0 +1,20 @@
+/*
+ * test.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: viktorb
+ */
+
+#ifndef INC_TEST_H_
+#define INC_TEST_H_
+
+
+
+#endif /* INC_TEST_H_ */
+
+void Test_program(void);
+
+void ShiftLED(uint8_t ledbuffer[], uint8_t bytesize);
+void Test_ShiftLeds(void);
+void testButtons(void);
+void testPL(void);
